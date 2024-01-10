cab4c54a4a6f4450af4ba71b96f479244556af4d
Travis Lamberte <travis@lamberte.com>
2024-01-10 14:31:10 -0800

    initial commit

--- routes/users.js
+++ routes/users.js
@@ -23,10 +23,10 @@ let users = [
     },
 ];
 
-// GET request: Retrieve all users
 router.get("/",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  const email = req.params.email
+  let filtered_users = users.filter((user) => user.email === email)
+  res.send(filtered_users)//This line is to be replaced with actual return value
 });
 
 // GET by specific ID request: Retrieve a single user with email ID
