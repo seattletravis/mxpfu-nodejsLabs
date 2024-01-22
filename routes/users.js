a339ef42602ccebad0dcd299a889436e78264da6
Travis Lamberte <travis@lamberte.com>
2024-01-22 13:35:39 -0800

    complete user.js file

--- routes/users.js
+++ routes/users.js
@@ -1,58 +1,82 @@
 const express = require('express');
+const { reset } = require('nodemon');
 const router = express.Router();
 
-
 let users = [
-    {
-        firstName: "John",
-        lastName: "wick",
-        email:"johnwick@gamil.com",
-        DOB:"22-01-1990",
-    },
-    {
-        firstName: "John",
-        lastName: "smith",
-        email:"johnsmith@gamil.com",
-        DOB:"21-07-1983",
-    },
-    {
-        firstName: "Joyal",
-        lastName: "white",
-        email:"joyalwhite@gamil.com",
-        DOB:"21-03-1989",
-    },
+	{
+		firstName: 'John',
+		lastName: 'wick',
+		email: 'johnwick@gamil.com',
+		DOB: '22-01-1990',
+	},
+	{
+		firstName: 'John',
+		lastName: 'smith',
+		email: 'johnsmith@gamil.com',
+		DOB: '21-07-1983',
+	},
+	{
+		firstName: 'Joyal',
+		lastName: 'white',
+		email: 'joyalwhite@gamil.com',
+		DOB: '21-03-1989',
+	},
 ];
 
-router.get("/",(req,res)=>{
-  res.send(users)
+// GET request: Retrieve all users
+router.get('/', (req, res) => {
+	res.send(users);
 });
 
 // GET by specific ID request: Retrieve a single user with email ID
-router.get("/:email",(req,res)=>{
-  const email = req.params.email
-  let filtered_users = users.filter((user) => user.email === email)
-  res.send(filtered_users)
+router.get('/:email', (req, res) => {
+	const email = req.params.email;
+	let filtered_users = users.filter((user) => user.email === email);
+	res.send(filtered_users);
 });
-
 
 // POST request: Create a new user
-router.post("/",(req,res)=>{
-  users.push({"firstName":req.query.firstName, "lastName":req.query.lastName, "email":req.query.email, "DOB":req.query.DOB})
-  res.send("The user" + (' ') + (req.query.firstName) + " Has been added!")
+router.post('/', (req, res) => {
+	users.push({
+		firstName: req.query.firstName,
+		lastName: req.query.lastName,
+		email: req.query.email,
+		DOB: req.query.DOB,
+	});
+	res.send('The user' + ' ' + req.query.firstName + ' Has been added!');
 });
-
 
 // PUT request: Update the details of a user by email ID
-router.put("/:email", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+router.put('/:email', (req, res) => {
+	const email = req.params.email;
+	let filtered_users = users.filter((user) => user.email === email);
+	if (filtered_users.length > 0) {
+		let filtered_user = filtered_users[0];
+		let DOB = req.query.DOB;
+		let firstName = req.query.firstName;
+		let lastName = req.query.lastName;
+		if (DOB) {
+			filtered_user.DOB = DOB;
+		}
+		if (firstName) {
+			filtered_user.firstName = firstName;
+		}
+		if (lastName) {
+			filtered_user.lastName = lastName;
+		}
+		users = users.filter((user) => user.email != email);
+		users.push(filtered_user);
+		res.send(`User with the email ${email} updated.`);
+	} else {
+		res.send('Unable to find user!');
+	}
 });
-
 
 // DELETE request: Delete a user by email ID
-router.delete("/:email", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+router.delete('/:email', (req, res) => {
+	const email = req.params.email;
+	users = users.filter((user) => user.email != email);
+	res.send(`User with email ${email} deleted.`);
 });
 
-module.exports=router;
+module.exports = router;
